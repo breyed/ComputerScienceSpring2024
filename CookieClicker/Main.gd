8b27aa6b26052a2b55ee89d13dc5af58ae1bfec8
Edward Brey <edward@breyfamily.net>
2024-02-21 13:34:13 -0600

    Multi-click button.

--- CookieClicker/Main.gd
+++ CookieClicker/Main.gd
@@ -2,11 +2,21 @@ extends Node2D
 
 var score = 0
 var points_per_click = 1
+var multi_click_price = 10
+
+func _ready():
+	add_to_score(0)
 
 func _on_cookie_button_pressed():
-	if score == 10:
-		points_per_click = 3
-	score = score  + points_per_click
-	$ScoreLabel.text = str(score)
+	add_to_score(points_per_click)
 
+func _on_multi_click_button_pressed():
+	points_per_click = points_per_click + 3
+	add_to_score(-multi_click_price)
+	
+func add_to_score(points):
+	score = score + points
+	$ScoreLabel.text = str(score)
+	$MultiClickButton.disabled = score < multi_click_price
 	$AudioStreamPlayer.play()
+	
