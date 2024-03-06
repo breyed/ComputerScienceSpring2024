dc22b4e571e830e6e791e465a69dfcb55167edca
Edward Brey <edward@breyfamily.net>
2024-03-06 15:47:03 -0600

    Granny at startup.

--- CookieClicker/Main.gd
+++ CookieClicker/Main.gd
@@ -3,9 +3,17 @@ extends Node2D
 var score = 0
 var points_per_click = 1
 var multi_click_price = 10
+var grannies = 0
+var frames = 0
 
 func _ready():
 	add_to_score(0)
+
+func _process(x):
+	frames = frames + 1
+	if frames == 60:
+		frames = 0
+		add_to_score(1)
 
 func _on_cookie_button_pressed():
 	add_to_score(points_per_click)
@@ -13,6 +21,9 @@ func _on_cookie_button_pressed():
 func _on_multi_click_button_pressed():
 	points_per_click = points_per_click + 3
 	add_to_score(-multi_click_price)
+
+func _on_granny_button_pressed():
+	grannies = grannies + 1
 	
 func add_to_score(points):
 	score = score + points
