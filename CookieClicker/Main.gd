6b7854b9799f61858bc49c5626181eb3451ca7b3
Edward Brey <edward@breyfamily.net>
2024-05-01 13:32:37 -0500

    Bonus payments.

--- CookieClicker/Main.gd
+++ CookieClicker/Main.gd
@@ -3,6 +3,8 @@ extends Node2D
 var score = 0
 var points_per_click = 1
 var multi_click_price = 10
+var granny_price = 30
+var grandpa_price = 50
 var grannies = 0
 var grandpas = 0
 var grandkids = 0
@@ -28,9 +30,11 @@ func _on_multi_click_button_pressed():
 
 func _on_granny_button_pressed():
 	grannies = grannies + 1
+	add_to_score(-granny_price)
 
 func _on_grandpa_button_pressed():
 	grandpas = grandpas + 2
+	add_to_score(-grandpa_price)
 	
 func add_to_score(points):
 	for grandkid in range(grandkids):
@@ -39,9 +43,12 @@ func add_to_score(points):
 	score = score + points
 	$ScoreLabel.text = str(score)
 	$Bonuses/Box/MultiClickButton.disabled = score < multi_click_price
+	$Bonuses/Box/GrannyButton.disabled = score < granny_price
+	$Bonuses/Box/GrandpaButton.disabled = score < grandpa_price
 	
 	# Show a flying cookie animation.
 	if points > 0:
+		$AudioStreamPlayer.play()
 		var flying_cookie = $FlyingCookieSprite.duplicate()
 		add_child(flying_cookie)
 		var player = flying_cookie.get_node("AnimationPlayer")
